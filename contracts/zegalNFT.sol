808ded1677cb88bf70eb1944c009bb29138b60c3
kirannbhatt <kiran.bhatt7638@gmail.com>
2021-11-03 12:26:53 +0545

    added document info in contract

--- contracts/zegalNFT.sol
+++ contracts/zegalNFT.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.3;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "base64-sol/base64.sol";
 
 contract zegalNFT is ERC721URIStorage, Ownable {
     uint256 public tokenCounter;
@@ -23,8 +24,8 @@ contract zegalNFT is ERC721URIStorage, Ownable {
     }
 
     function legalContractMeta(string memory document) public pure returns (string memory) {
-        string memory baseURL = "data:application/json;base64";
-        return string(abi.encodePacked(baseURL,Base64.encode(bytes(abi.encodePacked('{"name": "Zegal NFT", "description": "Legal document NFT", "url": "', document,'"}')))
+        string memory baseURL = "data:application/json;base64,";
+        return string(abi.encodePacked(baseURL, Base64.encode(bytes(abi.encodePacked('{"name": "Zegal NFT", "description": "Legal document NFT", "url": "', document,'"}')))
         ));
     }
 
