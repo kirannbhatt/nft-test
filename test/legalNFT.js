9b142e1c32845a3009d977e66eb8fe468a9dff38
Kiran Bhatt <kiran.bhatt7638@gmail.com>
2021-11-03 19:47:23 +0545

    updated test script

--- test/legalNFT.js
+++ test/legalNFT.js
@@ -5,18 +5,18 @@ describe("Minting the token and returning it", function () {
     const metadata =
       "https://opensea-creatures-api.herokuapp.com/api/creature/1"; //Random metadata url
 
-    const LegalContract = await ethers.getContractFactory("legalNFT"); // Getting the contract
+    const ZegalContract = await ethers.getContractFactory("zegalNFT"); // Getting the contract
 
-    const legalContract = await LegalContract.deploy(); //Deploying the Contract
+    const ZegalContract = await ZegalContract.deploy(); //Deploying the Contract
 
-    const transaction = await legalContract.mintToken(metadata); // Minting the token
+    const transaction = await ZegalContract.mintToken(metadata); // Minting the token
     const tx = await transaction.wait(); // Waiting for the token to be minted
 
     const event = tx.events[0];
     const value = event.args[2];
     const tokenId = value.toNumber(); // Getting the tokenID
 
-    const tokenURI = await legalContract.tokenURI(tokenId); // Using the tokenURI from ERC721 to retrieve de metadata
+    const tokenURI = await ZegalContract.tokenURI(tokenId); // Using the tokenURI from ERC721 to retrieve de metadata
 
     expect(tokenURI).to.be.equal(metadata); // Comparing and testing
   });
